11e3a59714bd4f6152380524345c72a113c6a404
young.shark8484@gmail.com <young.shark8484@gmail.com>
2024-05-20 16:44:25 +0300

    added form

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,63 @@
-
-import { Link} from 'react-router-dom';
-import MainRouter from './MainRouter';
-import { useState } from 'react';
-import { VLAD_ROUTE , LITUK_ROUTE, KOLYA_ROUTE, VASYA_ROUTE } from '../routes/config';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import {
+  VLAD_ROUTE,
+  LITUK_ROUTE,
+  KOLYA_ROUTE,
+  VASYA_ROUTE,
+  FORM_ROUTE,
+} from "../routes/config";
 
 type PropTypes = {
-    onDataChange: (childData: boolean) => void;
+  onDataChange: (childData: boolean) => void;
+};
+
+const Navbar: React.FC<PropTypes> = ({ onDataChange }) => {
+  const [isHide, setIsHide] = useState(false);
+
+  const sendDataToParent = () => {
+    setIsHide(!isHide);
+    onDataChange(isHide);
   };
 
-const Navbar: React.FC<PropTypes> = ({ onDataChange })=>{
-    
-    const [isHide, setIsHide] = useState(false);
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link className="link" to={VLAD_ROUTE}>
+            VLAD
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to={LITUK_ROUTE}>
+            LITUK
+          </Link>
+        </li>
 
-    const sendDataToParent = () => {
-        setIsHide(!isHide);
-        onDataChange(isHide);
-      };
+        <li>
+          <Link className="link" to={FORM_ROUTE}>
+            FORM
+          </Link>
+        </li>
 
-    return(
-
-        <div >
-            <ul>
-                <li><Link className='link' to={VLAD_ROUTE}>VLAD</Link></li>
-                <li><Link className='link' to={LITUK_ROUTE}>LITUK</Link></li>
-
-                    {isHide  && <><li><Link className='link' to={KOLYA_ROUTE}>KOLYA</Link></li>
-                    <li><Link className='link' to={VASYA_ROUTE}>VASYA</Link></li></>}
-                    
-
-                
-            </ul>
-            <button onClick={sendDataToParent}>Login</button>
-   
-        </div>
-    )
-}
+        {isHide && (
+          <>
+            <li>
+              <Link className="link" to={KOLYA_ROUTE}>
+                KOLYA
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to={VASYA_ROUTE}>
+                VASYA
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+      <button onClick={sendDataToParent}>Login</button>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,4 +1,5 @@
-export const LITUK_ROUTE = 'Lituk';
-export const VLAD_ROUTE = 'Vladick';
-export const VASYA_ROUTE = 'Vasya';
-export const KOLYA_ROUTE = 'Kolya';
+export const LITUK_ROUTE = "Lituk";
+export const VLAD_ROUTE = "Vladick";
+export const VASYA_ROUTE = "Vasya";
+export const KOLYA_ROUTE = "Kolya";
+export const FORM_ROUTE = "MyForm";
